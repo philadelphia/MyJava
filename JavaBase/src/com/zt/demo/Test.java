e0b683139727bb55363cc4d9c687725a83b779aa
tao.zt.zhang <tao.zt.zhang@deltaww.com>
2017-06-12 11:23:21 +0800

    添加一个JAVA集合相关的测试

--- JavaBase/src/com/zt/demo/Test.java
+++ JavaBase/src/com/zt/demo/Test.java
@@ -1,5 +1,11 @@
 package com.zt.demo;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -18,6 +24,57 @@ public class Test {
 		System.out.println(Student.class.getSimpleName());
 		System.out.println(Student.class.getPackage());
 		System.out.println(Student.class.getTypeParameters().toString());
+		
+		List<Student> students  = new ArrayList<>();
+		students.add(new Student(1, "dfg"));
+		students.add(new Student(2, "222"));
+		System.out.println(students);
+		students.get(0).setAge(12);
+//		此时集合中的数据已经被修改了。
+		System.out.println(students);
+//		被Collections集合类的unmodifiableCollection处理过的集合由于不提供get()方法。所以无法修改集合中的元素。
+//		变相的实现了不可变集合。
+//		unmodifiableCollection()该方法返回一个UnmodifiableCollection对象。
+//		UnmodifiableCollection类实现了Collection接口。没有get()方法，get是List集合自己的方法，
+		Collections.unmodifiableCollection(students);
+		System.out.println(students);
+		
+		
+		
+
+//		使用Arrays.asList(arrays)方法将数组变为集合的一个注意点是：修改此结合的数据同时会修改该数组中的元素。
+//		如果如要解决这个问题可以生成一个新的结合，然后使用Collections.addAll(list1, arrays);
+//		此时修改集合中的数据就不会影响数组中的元素了。
+	
+		
+		String[] arrays = new String[] {"hello", "World", "Hello", "Hadoop"};
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.print(arrays[i] + "\t");
+		}
+		System.out.println();
+		List<String> list = (List<String>) Arrays.asList(arrays);
+		list.set(0, "ttt");
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.print(arrays[i] + "\t");
+		}
+		System.out.println(Arrays.asList(arrays));
+		List<String> list1 = new ArrayList<>();
+		Collections.addAll(list1, arrays);
+		list1.set(0, "test");
+		for (int i = 0; i < arrays.length; i++) {
+			System.out.print(arrays[i] + "\t");
+		}
+		System.out.println();
+		System.out.println(Arrays.asList(arrays));
+		System.out.println(list1);
+
+//		Student s = new Student();
+//
+//		System.out.println(s instanceof Student);
+//		s = null;
+//		System.out.println(s instanceof Student);
+
+		
 	}
 
 }
